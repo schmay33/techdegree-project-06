e317d447fed36409b94a216defbab15bfd31e349
Nathan Schmidt <nschmdt@gmail.com>
2021-10-13 10:37:56 -0500

    Update descriptions

--- errorHandlers.js
+++ errorHandlers.js
@@ -14,11 +14,13 @@ const handleFourOhFour = (req, res, next) => {
 const handleGlobalError = (err, req, res, next) => {
   res.status(err.status || 500);
   if (err.status === 404) {
+    console.log("Status 404! Looks like this page does not exist.");
     res.render('page-not-found', {
       "message": err.message,
       "status": err.status
     });
   } else {
+    console.log(`Server Error! Status: ${err.status} Message: ${err.message}`);
     res.render('error', {
       "message": err.message,
       "error": err
